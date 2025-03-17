0c4a4ba73179d44062ccd6a86700c2d0f829ae32
TagBalu <baluyevslava@icloud.com>
2025-03-17 16:19:09 +0100

    primi esercizzi su java

---
+++ D1.js
@@ -0,0 +1,74 @@
+/*
+REGOLE
+- Tutte le risposte devono essere scritte in JavaScript
+- Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
+- Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
+- Per visualizzare l'output, lancia il file HTML a cui è collegato e apri la console dagli strumenti di sviluppo del browser. 
+- Utilizza dei console.log() per testare le tue variabili e/o i risultati delle espressioni che stai creando.
+*/
+
+/* ESERCIZIO 1
+ Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+// I datatype sono dei tipi di dati oppure formati con i quali usiamo per definire una variabile
+// definendo un certo valore che può essere: number, string, boolean, undefined e null.
+// Il number rapresenta dati decimali/non decimali. Lo string consiste in dati testuali racchiuse in carateri da citazioni.
+// Il boolean è il vero  o falso
+// undefined è una valore che non c'è al momento che pero in futuro si ha l'intezione di cambaire o no.
+// Il null invece è un valore nullo che si e dato volutamente.
+
+/* ESERCIZIO 2
+ Crea una variable chiamata "myName" e assegna ad essa il tuo nome, sotto forma di stringa.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+let myname;
+myname = "Vacisalv";
+console.log(myname);
+
+/* ESERCIZIO 3
+ Scrivi il codice necessario ad effettuare un addizione (una somma) dei numeri 12 e 20.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+const number1 = 20;
+const number2 = 12;
+console.log(number1 + number2);
+
+/* ESERCIZIO 4
+ Crea una variable di nome "x" e assegna ad essa il numero 12.
+*/
+
+/* SCRIVI QUI LA TUA RISPOSTA */
+let x = 12;
+
+/* ESERCIZIO 5
+  Riassegna un nuovo valore alla variabile "myName" già esistente: il tuo cognome.
+  Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const.
+*/
+/* SCRIVI QUI LA TUA RISPOSTA */
+
+myname = "Baluyev";
+console.log(myname);
+//D1.js:28 Uncaught SyntaxError: Missing initializer in const declaration (at D1.js:28:7)
+
+/* ESERCIZIO 6
+ Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
+*/
+/* SCRIVI QUI LA TUA RISPOSTA */
+console.log(4 - x);
+
+/* ESERCIZIO 7
+ Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
+ Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
+ EXTRA: verifica che la loro uguaglianza diventi true se entrambe vengono trasformate in lowercase (senza cambiare il valore di name2!).
+*/
+/* SCRIVI QUI LA TUA RISPOSTA */
+name1 = "john";
+name2 = "John";
+console.log("nome 1 è uguale a nome 2", name1 === name2);
+Lower = name2.toLowerCase();
+console.log("nome 1 è ugale a nome 2", name1 === Lower);
